89538945c3733fa6d8e89616a6a42a5ebfa92d4f
weinixiong <669075469@qq.com>
2019-04-27 18:01:37 +0800

    热力图

--- frontend/src/component/map/Heatmap.js
+++ frontend/src/component/map/Heatmap.js
@@ -1,100 +1,153 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React from 'react';
+import {
+  Chart,
+  Geom,
+  Axis,
+  Tooltip,
+  Label,
+} from 'bizcharts';
 
-import Heatmap_grid from './Heatmap_grid'
-import ReactDOM from 'react-dom'
+import { API_Heatmap_Grids } from '../../api/index'
 
-class Heatmap extends Component {
-  constructor(props) {
+let Config = {
+  1 : {
+     x_num: 30,
+     y_num: 15,
+     width: 640,
+     height: 368
+  },
+  2 : {
+     x_num: 12,
+     y_num: 15,
+     width: 280,
+     height: 368
+  }
+}
+
+export default class Heatmap extends React.Component {
+    constructor(props) {
     super(props);
     this.state = {
-        Heatmap_grids: [],
-        rectWidth:35,
-        rectHeight:35,
-        timer:0
+        data : [],
+        config:Config[1],
     };
-    this.svg = React.createRef()
   }
-componentDidMount(){//挂载后调用
-    this.getHeatmap_grids()
-  }
-  componentWillReceiveProps(nextProps){//接收父组件传入的props
-      this.getHeatmap_grids(nextProps)
-  }
-  getHeatmap_grids(nextProps){
-      let self = this
-    let { startHour,endHour,day,floor } = nextProps || this.props
-    let data = { startHour , endHour ,day,floor }
-       fetch('/api/Heatmap_grids',{
-        body: JSON.stringify(data),
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: 'same-origin', // include, same-origin, *omit
-          headers: {
-            'user-agent': 'Mozilla/4.0 MDN Example',
-            'content-type': 'application/json'
-          },
-          method: 'POST', // *GET, POST, PUT, DELETE, etc.
-          mode: 'cors', // no-cors, cors, *same-origin
-          redirect: 'follow', // manual, *follow, error
-          referrer: 'no-referrer', // *client, no-referrer
-       })
-        .then(r => r.json())
-        .then(response => {
-          	console.log(response)
-            self.drawHeatmap(response)
-        })
-  }
-  drawHeatmap(data){
-let { rectWidth,rectHeight } = this.state 
-    let { width ,height }  = this.props
-        const svg = this.svg.current
-         if (svg) {
-          svg.innerHTML=""
-          let nodes=[]
-          let max=0
-          let min=0
-          for(let i =1;i < data.length;i++){
-            if(max<data[i].count)
-              max=data[i].count
-            if(min>data[i].count||min==0)
-              min=data[i].count
-          }
-          let cha=max-min
-          for(let i =1;i < data.length;i++){
-            let color= "rgba(0,0,255,"+(data[i].count-min)/cha+")"
-           nodes.push(<Heatmap_grid x={data[i].x} y={data[i].y} color={color}>hello!</Heatmap_grid>)
-          }
-          ReactDOM.render(nodes ,svg)
-          
-  }
-    }
-  render() {
-    let { Heatmap_grids } = this.state 
-    let max=0
-    let min=0
-    Heatmap_grids.map((Heatmap_grid)=>{
-    	if(Heatmap_grid.count>max)
-    		max=Heatmap_grid.count
-    	if(Heatmap_grid.count<min ||min==0)
-    		max=Heatmap_grid.count
+  componentWillMount(){
+
+    // 可通过传入  startHour ,endHour,day 进行控制
+    // 同时需要调整相应的 后台
+
+    let { floor } = this.props 
+    let config = Config[floor]
+    this.setState({ 
+      config
     })
-    let cha=max-min
+
+    let data  = { 
+      startHour:8,
+      endHour:16,
+      day:1,
+      floor:floor
+    }
+
+    let self = this
+    API_Heatmap_Grids(data).then((res)=>{
+      // console.log(res)
+      for(let i =0; i < res.length;i++){
+        res[i]['y_reverse'] = 15 - res[i]['y']
+      }
+
+      self.setState({
+        data : res
+      })
+    })
+
+
+  }
+
+  render() {
+    let { config } = this.state
+
+    let x_values = [],
+        y_values =[]
+    for(let i =0;i < config.x_num;i++){
+      x_values.push(i)
+    }
+    for(let i = config.y_num;i >= 0;i--){
+      y_values.push(i)
+    }
+    const cols = {
+      x : {
+        type : 'cat',
+        values: x_values
+      },
+      y : {
+        type : 'cat',
+        values: y_values
+      },
+      y_reverse:{
+        type : 'cat',
+        values: y_values
+      }
+    }
+
+
     return (
-      <svg  className='map-Heatmap_grids'  ref={this.svg}
-        width={this.props.width} height={this.props.height} >
-          
-      </svg>
+      <div style={{ 'display':'inline-block', 'width':config.width , 'height':config.height  }}>
+        <Chart
+          width={config.width}
+          height={config.height - 9}
+          data={this.state.data}
+          scale={cols}
+          padding={[ 20, 20, 20, 20]}
+        >
+          <Axis
+            name="x"
+            grid={{
+              align: 'center',
+              lineStyle: {
+                lineWidth: 1,
+                lineDash: null,
+                stroke: '#f0f0f0',
+              },
+              showFirstLine: true,
+            }}
+          />
+          <Axis
+            name="y"
+            grid={{
+              align: 'center',
+              lineStyle: {
+                lineWidth: 1,
+                lineDash: null,
+                stroke: '#f0f0f0',
+              },
+              showFirstLine: true,
+            }}
+          />
+          <Tooltip title="位置:人数"/>
+          <Geom
+            type="polygon"
+            position="x*y_reverse"
+            color={['count', '#BAE7FF-#1890FF-#0050B3']}
+            style={{
+              stroke: '#fff',
+              lineWidth: 1,
+              opacity: 0.3
+            }}
+
+             tooltip={['x*y*count', (x, y,count) => {
+                return {
+                  //自定义 tooltip 上显示的 title 显示内容等。
+                  name: `(${x},${y})`,
+                  value: count
+                };
+              }]}
+          >
+          </Geom>
+        </Chart>
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    startHour: state.startHour,
-    endHour: state.endHour,
-    day: state.day,
-  }
-}
-
-
-export default connect(mapStateToProps)(Heatmap)
