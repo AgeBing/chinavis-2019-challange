4ac6ea0f526fe85cbeec0d9be547408025f10dda
weinixiong <669075469@qq.com>
2019-06-01 23:30:16 +0800

    修正热力图存在得数据不精确的问题

--- server/src/model/Heatmap_grids.js
+++ server/src/model/Heatmap_grids.js
@@ -3,20 +3,39 @@ const query      = require('./db.js')
 class Heatmap_grids{
   async getHeatmap_grids(Heatmap_minutes,day,floor) {//Heatmap_minutes:550,范围:550整个一分钟
       let tableName  = 'trajectories_inf'+day//id,sid,time,floor,x,y
-      // let start_time=Heatmap_minutes/60+":"+Heatmap_minutes%60+":00"
-      let end_=Heatmap_minutes+1
-      // let end_time=end_/60+":"+end_%60+":00"
-      let sql = `SELECT x,y,count(id) as count FROM ${tableName} WHERE 
-        (HOUR(time)*60 + MINUTE(time))  >= ${Heatmap_minutes} AND
-         (HOUR(time)*60 + MINUTE(time))  <= ${(Heatmap_minutes + 1)} 
+      let sql = `SELECT x,y,(HOUR(time)*60 + MINUTE(time)) as t_minutes,id FROM ${tableName} WHERE 
+        (HOUR(time)*60 + MINUTE(time))  >= ${Heatmap_minutes-20} AND
+         (HOUR(time)*60 + MINUTE(time))  <= ${(Heatmap_minutes +20)} 
           AND floor=${floor}
-         group by x,y` 
+         order by id,time` //group by x,y
 
       let dataList = await query( sql )
-      // console.log(dataList)
-      // console.log(dataList.length)
-      console.log('dataList',dataList)
-    return await dataList;
+      let new_datalist={}//{'x,y':count}
+      let bef_id=''//上一个找到正确记录的id
+      for (let i=1;i<dataList.length;i++)
+      {
+        //当前记录的id不是已找到正确记录的最新的id
+        if (bef_id!=dataList[i]['id'] && (dataList[i-1]['id']==dataList[i]['id']) && (parseInt(dataList[i]['t_minutes'])>parseInt(Heatmap_minutes)))
+          {
+            
+            bef_id=dataList[i-1]['id']//更新当前已找到正确记录的id
+            let cur_key=dataList[i-1]['x']+','+dataList[i-1]['y']
+            if(!new_datalist.hasOwnProperty(cur_key)){
+              new_datalist[cur_key] = 1
+            }
+            else
+              new_datalist[cur_key] += 1
+          }
+      }
+      let sids= Object.keys(new_datalist) 
+      let result=[]
+      sids.forEach((sid_str)=>{//sid_str:'x,y'
+        let sid=sid_str.split(',')
+result.push([parseInt(sid[0]),parseInt(sid[1]),new_datalist[sid_str]])
+      })
+      
+      // console.log('dataList finish',dataList.length,sids.length,result)
+    return await result;
   }
 }
 
