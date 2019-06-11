01affdc53300e80569d8f1ee4c556cebab04e9e8
Age <ykjage@gmail.com>
2019-06-11 14:54:05 +0800

    添加文档，去除敏感信息

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+ChinaVis2019 挑战赛 系统实现
+
 ## 0. analysis/
 
 Python 脚本
@@ -13,20 +15,10 @@ KOA
 
 ## 3. Database
 
-```
-host     : '115.159.202.238',
-user     : 'chinavis',
-password : 'chinavis2019',
-database : 'chinavis2019'
-```
 ## 4. Development
+
 ```
 # 当前目录下
 npm install
 npm start
 ```
-
-## 5. 数据预处理
-
-> 通过运行`analysis`中的`analysis.py`文件可以将 data 文件夹中的`day1.csv`以一个小时为单位进行分割。分割的文件名表示的是秒数，包含该时间开始以后一个小时内的轨迹。
-> 通过用`analysis.py`中的`processCustom(filename,targetDir)`方法可以自定义源文件和目标文件路径。
--- server/src/model/db.js
+++ server/src/model/db.js
@@ -1,20 +1,20 @@
 const mysql      = require('mysql')
 const poolRemote = mysql.createPool({
-  host     : '115.159.202.238',   // 数据库地址
-  user     : 'chinavis',    // 数据库用户
-  password : 'chinavis2019' ,  // 数据库密码
-  database : 'chinavis2019'  // 选中数据库
+  host     : '',   // 数据库地址
+  user     : '',    // 数据库用户
+  password : '' ,  // 数据库密码
+  database : ''  // 选中数据库
 })
 
 const poolLocal = mysql.createPool({   //本地
-  host     : '127.0.0.1',  
-  user     : 'root',    
-  password : 'qweasd9095' ,  // 数据库密码 改成自己的密码
-  database : 'chinavis2019'  // 选中数据库
+  host     : '',  
+  user     : '',    
+  password : '' ,  // 数据库密码 改成自己的密码
+  database : ''  // 选中数据库
 })
 
-let pool = poolRemote
-// let pool = poolLocal
+// let pool = poolRemote
+let pool = poolLocal
 
 let query = function( sql, values ) {
   return new Promise(( resolve, reject ) => {
